a99ca566ccdbaa36026486aa20a9e9f1956d5650
aillean <aillean@126.com>
2021-06-07 22:51:06 +0800

    升级到了 android x 编译jar 进去了。提示虚拟机无法启动。

--- app/src/main/java/com/aillean/main/MainActivity.java
+++ app/src/main/java/com/aillean/main/MainActivity.java
@@ -1,7 +1,7 @@
 package com.aillean.main;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
